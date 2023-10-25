2abc24b178c659724bed8034575d84d0d64ab9f9
Karatkevich Aliaksei <torlesh18112003@gmail.com>
2023-10-26 01:51:53 +0300

    feat: all except 21, 21

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -1,18 +1,14 @@
 package by.clevertec;
 
-import by.clevertec.model.Animal;
-import by.clevertec.model.Car;
-import by.clevertec.model.Examination;
-import by.clevertec.model.Flower;
-import by.clevertec.model.House;
-import by.clevertec.model.Person;
-import by.clevertec.model.Student;
+import by.clevertec.model.*;
 import by.clevertec.util.Util;
 
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -31,13 +27,13 @@ public class Main {
         //task11();
         //task12();
         //task13();
-        task14();
-        task15();
+        //task14();
+        //task15();
         //task16();
         //task17();
-        task18();
-        task19();
-        task20();
+        //task18();
+        //task19();
+        //task20();
         task21();
         task22();
     }
@@ -56,14 +52,10 @@ public class Main {
         List<Animal> animals = Util.getAnimals();
         animals.stream()
                 .filter(animal -> animal.getOrigin().equals("Japanese"))
-                .peek(animal -> {
-                    if (animal.getGender().equals("Female")) {
-                        animal.setBread(animal.getBread().toUpperCase());
-                    }
-                })
+                .peek(animal -> animal.setBread(animal.getBread().toUpperCase()))
+                .filter(animal -> animal.getGender().equals("Female"))
                 .map(Animal::getBread)
                 .forEach(System.out::println);
-
     }
 
     public static void task3() {
@@ -154,6 +146,7 @@ public class Main {
 
     public static void task13() {
         List<House> houses = Util.getHouses();
+
         houses.stream()
                 .flatMap(house -> house.getPersonList().stream()
                         .map(person -> {
@@ -177,16 +170,75 @@ public class Main {
 
     public static void task14() {
         List<Car> cars = Util.getCars();
-//        cars.stream()
-//                .map(car -> {
-//
-//                })
+
+        Predicate<Car> isJaguarOrWhite = car -> car.getCarMake().equals("Jaguar") || car.getColor().equals("White");
+        Predicate<Car> isBMWOrLexusOrChryslerOrToyotaUnder1500 = car ->
+                (car.getMass() < 1500) && (car.getCarMake().equals("BMW")
+                        || car.getCarMake().equals("Lexus")
+                        || car.getCarMake().equals("Chrysler")
+                        || car.getCarMake().equals("Toyota")
+                );
+        Predicate<Car> isBlackAndOver4000OrGMCOrDodge = car ->
+                (car.getColor().equals("Black") && car.getMass() > 4000)
+                        || car.getCarMake().equals("GMC")
+                        || car.getCarMake().equals("Dodge");
+        Predicate<Car> isBefore1982OrCivicOrCherokee = car ->
+                (car.getReleaseYear() < 1982)
+                        || car.getCarMake().equals("Civic")
+                        || car.getCarMake().equals("Cherokee");
+        Predicate<Car> isNotYRGBColorOrOver40000 = car ->
+                !car.getColor().equals("Yellow")
+                        && !car.getColor().equals("Red")
+                        && !car.getColor().equals("Green")
+                        && !car.getColor().equals("Blue")
+                        || car.getPrice() > 40000;
+        Predicate<Car> contains59InVin = car -> car.getVin().contains("59");
+
+        System.out.println(
+                cars.stream()
+                        .collect(Collectors.groupingBy(car -> {
+                                    if (isJaguarOrWhite.test((Car) car)) return 1;
+                                    if (isBMWOrLexusOrChryslerOrToyotaUnder1500.test((Car) car)) return 2;
+                                    if (isBlackAndOver4000OrGMCOrDodge.test((Car) car)) return 3;
+                                    if (isBefore1982OrCivicOrCherokee.test((Car) car)) return 4;
+                                    if (isNotYRGBColorOrOver40000.test((Car) car)) return 5;
+                                    if (contains59InVin.test((Car) car)) return 6;
+                                    return 7;
+                                })
+                        )
+                        .entrySet().stream()
+                        .filter(entry -> entry.getKey() != 7)
+                        .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue)
+                        )
+                        .values().stream()
+                        .mapToDouble(carsList -> carsList.stream()
+                                .mapToDouble(Car::getMass)
+                                .sum() * 0.00714)
+                        .peek(System.out::println)
+                        .sum()
+        );
 
     }
 
     public static void task15() {
         List<Flower> flowers = Util.getFlowers();
-//        flowers.stream() Продолжить ...
+        System.out.println(flowers.stream()
+                .sorted(Comparator.comparing(Flower::getOrigin).reversed()
+                        .thenComparing(Flower::getPrice)
+                        .thenComparing(Flower::getWaterConsumptionPerDay).reversed())
+                .filter(flower -> flower.getCommonName().charAt(0) >= 'C'
+                    && flower.getCommonName().charAt(0) <= 'S')
+                .filter(Flower::isShadePreferred)
+                .filter(flower -> flower.getFlowerVaseMaterial().contains("Glass")
+                        || flower.getFlowerVaseMaterial().contains("Aluminum")
+                        || flower.getFlowerVaseMaterial().contains("Steel"))
+                .mapToDouble(flower -> flower.getPrice() + 5 * 365 * flower.getWaterConsumptionPerDay() * 0.00139)
+                .sum()
+        );
+
+
     }
 
     public static void task16() {
@@ -210,18 +262,49 @@ public class Main {
 
     public static void task18() {
         List<Student> students = Util.getStudents();
-        List<Examination> examinations = Util.getExaminations();
-//        students.stream() Продолжить ...
+        students.stream()
+                .collect(Collectors.groupingBy(Student::getFaculty,
+                        Collectors.averagingDouble(Student::getAge))
+                )
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+
     }
 
     public static void task19() {
         List<Student> students = Util.getStudents();
-//        students.stream() Продолжить ...
+        List<Examination> examinations = Util.getExaminations();
+        System.out.print("Введите номер группы: ");
+        Scanner sc = new Scanner(System.in);
+        String group = sc.next();
+        System.out.println(students.stream()
+                .filter(student -> student.getGroup().equalsIgnoreCase(group))
+                .filter(student -> examinations.stream()
+                        .filter(exam -> exam.getStudentId() == student.getId())
+                        .allMatch(exam -> exam.getExam3() > 4)
+                )
+                .toList()
+        );
     }
 
     public static void task20() {
         List<Student> students = Util.getStudents();
-//        students.stream() Продолжить ...
+        List<Examination> examinations = Util.getExaminations();
+        students.stream()
+                .collect(Collectors.groupingBy(Student::getFaculty,
+                        Collectors.averagingDouble(student ->
+                            examinations.stream().
+                                    filter(examination -> examination.getStudentId() == student.getId())
+                                    .mapToDouble(Examination::getExam1)
+                                    .findFirst()
+                                    .orElse(0.0)
+                        ))
+                )
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .ifPresent(System.out::println);
     }
 
     public static void task21() {
